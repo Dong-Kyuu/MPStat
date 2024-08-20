11d6e0321a920b79398d65c575f27bf3305ee2cb
동규 <iopz8811@gmail.com>
2024-08-20 16:00:59 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/statddal/StatDdalApplication.java
@@ -0,0 +1,13 @@
+package com.example.statddal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatDdalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatDdalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/statddal/StatDdalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.statddal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatDdalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
